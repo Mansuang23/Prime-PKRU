a66117880b56b3a2a73615867aec6b1ab6bb3675
Doramon <abc@gmail.com>
2017-07-27 11:05:16 +0700

    test

---
+++ Prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime
+//
+//  Created by Com 05 on 7/27/2560 BE.
+//  Copyright © 2560 Com 05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
